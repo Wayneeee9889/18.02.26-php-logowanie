7ab45ff6f1dcd2181bdf50fe50d39cbb10e1ff1b
Waynee <uwusiara9889@gmail.com>
2026-02-23 13:07:25 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,11 @@
 <?php
-session_start();
-
-//połączenie z bazą danych
+//Połączenie z serwerzem
 $con = mysqli_connect("localhost", "root", "", "blogg");
-//sprawdzenie czy jest połączenie z bazą danych, jeżeli nie to wyswietli się nam "nie udało sie połaczyć z bazą danych"
-if($con->errno){
-    echo "nie udało sie połaczyć z bazą danych";
+if ($con->errno){
+    echo "nie udało się połączyć z bazą dannych";
     exit;
 }else{
-    echo "Połączono";
+    echo "połąnczono";
 }
+
+?>
